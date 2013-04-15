365dfc2b5dbf5ecb55d1eb4c2ac1ee821b6e2541
Kris Moore <kris@pcbsd.org>
2013-04-15 13:17:08 -0400

    - Fix a bug converting jails to new meta format, use unique IDs

--- src-sh/warden/scripts/backend/listjails.sh
+++ src-sh/warden/scripts/backend/listjails.sh
@@ -60,8 +60,21 @@ do
      	continue
      fi
      # This is an old style jail, lets convert it
-     cp ${i}/ip ${i}/id
      cp ${i}/ip ${i}/ipv4
+
+     # Get next unique ID
+     META_ID=0
+     for iDir in `ls -d ${JDIR}/.*.meta 2>/dev/null`
+     do
+       if [ ! -e "${iDir}/id" ] ; then continue ; fi
+       id=`cat ${iDir}/id`
+       if [ "${id}" -gt "${META_ID}" ] ; then
+          META_ID="${id}"
+       fi
+     done
+     : $(( META_ID += 1 ))
+     echo "$META_ID" > ${i}/id
+
   fi
 
   ID="`cat ${i}/id 2>/dev/null`"
