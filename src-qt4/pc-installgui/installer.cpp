a10fe9c6ab52ff5c936f542ddb0ac73c4db19418
Kris Moore <kris@pcbsd.org>
2013-05-07 18:10:57 -0400

    Further cleanup pkgng install output

--- src-qt4/pc-installgui/installer.cpp
+++ src-qt4/pc-installgui/installer.cpp
@@ -1504,7 +1504,7 @@ void Installer::slotReadInstallerOutput()
 
 	// If we are installing a package, show details to user
 	if ( tmp.indexOf("Installing") == 0 )
-          labelInstallStatus2->setText(tmp.section(" ", 0, 1));
+          labelInstallStatus2->setText(tmp.section("...", 0, 0));
      }
 
   } // end of while loop
