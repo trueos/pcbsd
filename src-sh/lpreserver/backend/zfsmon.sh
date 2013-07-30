ca25b679c8c4b9ae262848e4e73f6f10714eb326
Kris Moore <kris@pcbsd.org>
2013-07-30 18:33:12 -0400

    Add the placeholder for our ZFS monitor
    Add some additional logging messages if a snapshot create / prune fails

---
+++ src-sh/lpreserver/backend/zfsmon.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Do the cron snapshot
+######################################################################
+
+# Set our vars
+PROGDIR="/usr/local/share/lpreserver"
+
+# Source our functions
+. /usr/local/share/pcbsd/scripts/functions.sh
+. ${PROGDIR}/backend/functions.sh
+
+# Placeholder for now, will be our ZFS monitoring stuff here
