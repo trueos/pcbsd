1db1c9b454bd8acabfd700a01ff6ccf81d2b7b0c
Kris Moore <kris@pcbsd.org>
2013-09-01 14:41:57 -0400

    Use a different working dir when checking for updates on this tray, to avoid collisions with other instances of "freebsd-update"

--- src-sh/pc-fbsdupdatecheck/main.c
+++ src-sh/pc-fbsdupdatecheck/main.c
@@ -9,7 +9,8 @@
 int main()
 {
 	system("touch /tmp/.fbsdup-lock");
-	system("cat /usr/sbin/freebsd-update | sed 's|! -t 0|-z '1'|g' | /bin/sh -s 'fetch'");
+        system("mkdir /var/db/freebsd-update-check 2>/dev/null");
+	system("cat /usr/sbin/freebsd-update | sed 's|! -t 0|-z '1'|g' | /bin/sh -s 'fetch' '-d' '/var/db/freebsd-update-check'");
 	system("rm /tmp/.fbsdup-lock");
 	return 0;
 }
