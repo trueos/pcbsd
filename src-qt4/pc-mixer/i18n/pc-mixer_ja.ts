fabdcd270eeba692851ff49d5880edb6a46e1b72
Ken Moore <ken@pcbsd.org>
2014-03-07 13:47:15 -0500

    Add the pc-mixer translation files i18n/*.ts

---
+++ src-qt4/pc-mixer/i18n/pc-mixer_ja.ts
@@ -0,0 +1,74 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ja_JP">
+<context>
+    <name>DeviceWidget</name>
+    <message>
+        <location filename="../DeviceWidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../DeviceWidget.ui" line="85"/>
+        <source>L</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../DeviceWidget.ui" line="95"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../DeviceWidget.ui" line="106"/>
+        <source>R</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../DeviceWidget.ui" line="127"/>
+        <location filename="../DeviceWidget.cpp" line="63"/>
+        <source>Mute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../DeviceWidget.cpp" line="60"/>
+        <source>Unmute</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MixerGUI</name>
+    <message>
+        <location filename="../MixerGUI.ui" line="14"/>
+        <source>Mixer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MixerGUI.ui" line="29"/>
+        <source>Default Tray Device:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MixerGUI.ui" line="68"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MixerGUI.ui" line="83"/>
+        <source>Close Mixer and Tray</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MixerGUI.ui" line="92"/>
+        <source>Hide Mixer</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MixerTray</name>
+    <message>
+        <location filename="../MixerTray.cpp" line="18"/>
+        <source>Mixer</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
