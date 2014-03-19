8a5b8ec636dcf49c149dbfeca966a8d423972b03
Kris Moore <kris@pcbsd.org>
2014-03-19 13:42:08 -0400

    Enable lz4 compression on ZFS root

--- src-qt4/pc-installgui/wizardDisk.cpp
+++ src-qt4/pc-installgui/wizardDisk.cpp
@@ -420,7 +420,9 @@ void wizardDisk::generateDiskLayout()
 
     QString rootOpts="";
     if ( checkSSD->isChecked() )
-	rootOpts="(atime=off)";
+	rootOpts="(compress=lz4|atime=off)";
+    else
+	rootOpts="(compress=lz4)";
 
      // This lets the user do nifty stuff like a mirror/raid post-install with a single zpool command
     fileSystem << targetDisk << targetSlice << "/" + rootOpts + ",/tmp(compress=lz4),/usr(canmount=off),/usr/home(compress=lz4),/usr/jails(compress=lz4),/usr/obj(compress=lz4),/usr/pbi(compress=lz4),/usr/ports(compress=lz4),/usr/ports/distfiles(compress=lz4),/usr/src(compress=lz4),/var(canmount=off),/var/audit(compress=lz4),/var/log(compress=lz4),/var/tmp(compress=lz4)" << fsType << tmp.setNum(totalSize) << "" << tmpPass;
