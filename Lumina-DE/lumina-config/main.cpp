b3191325a3e430afd2cb1229f4a00a6b1da31984
JoshDW19 <joshms@pcbsd.org>
2014-04-16 11:29:44 -0400

    Modified file to include fixes from Ken.

--- Lumina-DE/lumina-config/main.cpp
+++ Lumina-DE/lumina-config/main.cpp
@@ -10,14 +10,13 @@ int main(int argc, char ** argv)
     QtSingleApplication a(argc, argv);
     if( a.isRunning() )
       return !(a.sendMessage("show"));
-
-    
+  
     QTranslator translator;
     QLocale mylocale;
     QString langCode = mylocale.name();
     
-    if ( ! QFile::exists(PREFIX + "/share/Lumina-DE/lumina-config_" + langCode + ".qm" ) )  langCode.truncate(langCode.indexOf("_"));
-    translator.load( QString("lumina-config_") + langCode, PREFIX + "/share/Lumina-DE/" );
+    if ( ! QFile::exists(PREFIX + "/share/lumina/i18n/" + langCode + ".qm" ) )  langCode.truncate(langCode.indexOf("_"));
+    translator.load( QString("lumina-config_") + langCode, PREFIX + "/share/lumina/i18n/" );
     a.installTranslator( &translator );
     qDebug() << "Locale:" << langCode;
     
@@ -26,8 +25,6 @@ int main(int argc, char ** argv)
     QObject::connect(&a, SIGNAL(messageReceived(const QString&)), &w, SLOT(slotSingleInstance()) );
     w.show();
 
-      }
-    }
     int retCode = a.exec();
     return retCode;
 }
