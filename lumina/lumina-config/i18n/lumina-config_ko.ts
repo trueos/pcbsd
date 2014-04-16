554eae89ee062e5760dfcb899c2a6acb1d526913
Ken Moore <ken@pcbsd.org>
2014-04-16 12:09:58 -0400

    Add the other template files lumina-config.pro, i18n directory with translation files

---
+++ lumina/lumina-config/i18n/lumina-config_ko.ts
@@ -0,0 +1,87 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ko_KR">
+<context>
+    <name>MainUI</name>
+    <message>
+        <location filename="../mainUI.ui" line="14"/>
+        <source>Desktop Configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="32"/>
+        <source>Screen Number:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="64"/>
+        <source>Background</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="97"/>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="104"/>
+        <source>Rem</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="124"/>
+        <source>Single Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="131"/>
+        <source>Rotate Images</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="138"/>
+        <source>  Minutes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="141"/>
+        <source>Every  </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="157"/>
+        <source>Desktop Plugins</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="162"/>
+        <source>Panels</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="185"/>
+        <source>&amp;Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="192"/>
+        <source>&amp;Save Changes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="211"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="221"/>
+        <source>Save and &amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="226"/>
+        <source>&amp;Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
