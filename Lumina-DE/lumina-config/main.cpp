e06949d305822d7c0d194ea6e944cf0dc83b7b98
JoshDW19 <joshms@pcbsd.org>
2014-04-16 11:06:12 -0400

    main.cpp file modified for lumina-config

---
+++ Lumina-DE/lumina-config/main.cpp
@@ -0,0 +1,33 @@
+#include <QTranslator>
+#include <qtsingleapplication.h>
+#include <QtGui/QApplication>
+#include <QDebug>
+#include "MainUI.h"
+#define PREFIX QString("/usr/local")
+
+int main(int argc, char ** argv)
+{
+    QtSingleApplication a(argc, argv);
+    if( a.isRunning() )
+      return !(a.sendMessage("show"));
+
+    
+    QTranslator translator;
+    QLocale mylocale;
+    QString langCode = mylocale.name();
+    
+    if ( ! QFile::exists(PREFIX + "/share/Lumina-DE/lumina-config_" + langCode + ".qm" ) )  langCode.truncate(langCode.indexOf("_"));
+    translator.load( QString("lumina-config_") + langCode, PREFIX + "/share/Lumina-DE/" );
+    a.installTranslator( &translator );
+    qDebug() << "Locale:" << langCode;
+    
+
+    MainUI w;
+    QObject::connect(&a, SIGNAL(messageReceived(const QString&)), &w, SLOT(slotSingleInstance()) );
+    w.show();
+
+      }
+    }
+    int retCode = a.exec();
+    return retCode;
+}
