441a9b7c51716c2471d6b9b5d85cb4038c59ed3b
Ken Moore <ken@pcbsd.org>
2014-05-14 15:51:04 -0400

    Minor correction for the panel: don't need the extra 2 pixel buffer when reserving the panel location now - the recent LTBWidget updates fixed that.

--- lumina/lumina-desktop/LPanel.cpp
+++ lumina/lumina-desktop/LPanel.cpp
@@ -58,10 +58,10 @@ void LPanel::UpdatePanel(){
   qDebug() << " - set Geometry";
   if(loc=="top"){
     this->setGeometry(xoffset,0,screen->screenGeometry(screennum).width(), ht );
-    LX11::ReservePanelLocation(this->winId(), xoffset, 0, this->width(), ht+2);
+    LX11::ReservePanelLocation(this->winId(), xoffset, 0, this->width(), ht);
   }else{
     this->setGeometry(xoffset,screen->screenGeometry(screennum).height()-ht,screen->screenGeometry(screennum).width(), ht );
-    LX11::ReservePanelLocation(this->winId(), xoffset, screen->screenGeometry(screennum).height()-ht, this->width(), ht+2);
+    LX11::ReservePanelLocation(this->winId(), xoffset, screen->screenGeometry(screennum).height()-ht, this->width(), ht);
   }
   //Now update the appearance of the toolbar
   QString color = settings->value(PPREFIX+"color", "rgb(255,250,250)").toString();
