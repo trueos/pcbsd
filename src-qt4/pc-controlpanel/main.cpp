1eae2bdba527331fe680a91cb4b770e63c70bac0
yurkis <yurkis@gmail.com>
2014-06-02 14:53:43 +0300

    Control panel: quick fix loading translations. I need to check PCSingleApplication class from libpcbsd-ui in case of translations loading

--- src-qt4/pc-controlpanel/main.cpp
+++ src-qt4/pc-controlpanel/main.cpp
@@ -7,10 +7,18 @@
 
 int main(int argc, char *argv[])
 {        
-    PCSingleApplication a(argc, argv);
+    PCSingleApplication a(argc, argv, "pc-controlpanel");
 
     if ( a.isRunning() )
               return !(a.sendMessage("show"));
+
+    QTranslator translator;
+    QLocale mylocale;
+    QString langCode = mylocale.name();
+    if ( ! QFile::exists( PREFIX + "/share/pcbsd/i18n/pc-controlpanel_" + langCode + ".qm" ) )
+              langCode.truncate(langCode.indexOf("_"));
+    translator.load( QString("pc-controlpanel_") + langCode, PREFIX + "/share/pcbsd/i18n/" );
+    a.installTranslator( &translator );
 
     MainWindow w;       
 
