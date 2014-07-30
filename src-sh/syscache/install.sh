9255a4d8f1f5f3de3a9daf8d34ff63ec4540a876
Kris Moore <kris@pcbsd.org>
2014-07-30 15:34:09 -0400

    Link syscache into the build of src-sh, add rc.d script

---
+++ src-sh/syscache/install.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+# Warden install script
+
+DEFAULT="/usr/local"
+
+if [ -z "$1" ] ; then
+        LB="${DEFAULT}"
+else
+        LB="${1}"
+fi
+
+DESTDIR=${LB} ; export DESTDIR
+
+cd daemon
+if [ $? -ne 0 ] ; then exit 1 ; fi
+qmake-qt4 daemon.pro
+if [ $? -ne 0 ] ; then exit 1 ; fi
+make
+if [ $? -ne 0 ] ; then exit 1 ; fi
+make install
+if [ $? -ne 0 ] ; then exit 1 ; fi
+
+cd ../client
+if [ $? -ne 0 ] ; then exit 1 ; fi
+qmake-qt4 client.pro
+if [ $? -ne 0 ] ; then exit 1 ; fi
+make
+if [ $? -ne 0 ] ; then exit 1 ; fi
+make install
+if [ $? -ne 0 ] ; then exit 1 ; fi
+
+if [ ! -d "${LB}/etc/rc.d" ] ; then
+   mkdir -p ${LB}/etc/rc.d
+fi
+
+cd ../
+if [ $? -ne 0 ] ; then exit 1 ; fi
+cp rc.d/syscache ${LB}/etc/rc.d/syscache
+if [ $? -ne 0 ] ; then exit 1 ; fi
+chmod 755 ${LB}/etc/rc.d/syscache
+if [ $? -ne 0 ] ; then exit 1 ; fi
+
+exit 0
