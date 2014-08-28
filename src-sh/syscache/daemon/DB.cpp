64470b0e6658b5ec165e49673b580e1f0022d468
Ken Moore <ken@pcbsd.org>
2014-08-28 11:11:37 -0400

    Add a check for the local system to resync the local info if the installed pkg list is different (don't just rely on pkg database timestamps, they seem erratic)

--- src-sh/syscache/daemon/DB.cpp
+++ src-sh/syscache/daemon/DB.cpp
@@ -310,7 +310,9 @@ bool Syncer::needsLocalSync(QString jail){
       QString path = "/var/db/pkg/local.sqlite";
       qint64 mod = QFileInfo(path).lastModified().toMSecsSinceEpoch();
       qint64 stamp = HASH->value("Jails/"+jail+"/lastSyncTimeStamp","").toLongLong();
-      return (mod > stamp); //was it modified after the last sync?
+      if(mod > stamp){ return true; }//was it modified after the last sync?
+      //Otherwise check if the installed pkg list if different (sometimes timestamps don't get updated properly on files)
+      return (HASH->value("Jails/"+jail+"/pkgList","") != directSysCmd("pkg query -a %o").join(LISTDELIMITER) );
     }else{
       //This is inside a jail - need different method
       //Check that the list of installed pkgs has not changed (should be very fast)
