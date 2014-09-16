a71d7c5d0090b793287f0abafe712b8489085ce9
Kris Moore <kris@pcbsd.org>
2014-09-16 11:55:37 -0400

    Update irc/bitlbee with service configuration data for AppCafe

--- pbi-modules/irc/bitlbee/resources/services/bitlbee/disable.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# This script disables the service at bootup
-# Return 0 on success, or 1 on failure. 
-
-cat /etc/rc.conf | grep -v "bitlbee_enable=" >/tmp/rc.new
-echo "bitlbee_enable=\"NO\"" >> /tmp/rc.new
-mv /tmp/rc.new /etc/rc.conf
-
-return 0
