91423998b67bb2f7e2264c1f10ab12d45dfb1703
Bjarne Bertilsson <bjarne.bertilsson@gmail.com>
2014-09-19 16:53:20 +0200

    Fixed bug. Cleanup

--- src-sh/lpreserver/backend/zfslistcronscrub.sh
+++ src-sh/lpreserver/backend/zfslistcronscrub.sh
@@ -16,16 +16,10 @@ echo "---------------------------------"
 for i in `grep "${PROGDIR}/backend/runscrub.sh" /etc/crontab | awk '{print $8}'`
 do
    hour=`grep "${PROGDIR}/backend/runscrub.sh ${i}" /etc/crontab | awk '{print $2}'`
-   day=`grep "${PROGDIR}/backend/runscrub.sh ${i}" /etc/crontab | awk '{print $5}'`
-   month=`grep "${PROGDIR}/backend/runscrub.sh ${i}" /etc/crontab | awk '{print $3}'`
-   if [ "$day" = '*' -a "$month" = '*' ] ; then time="daily @ hour $hour" ; fi
-   if [ "$day" != '*' ] ; then time="weekly @ day $day" ; fi
-   if [ "$month" != '*' ] ; then time="monthly @ day $day @ hour $hour" ; fi
+   day_week=`grep "${PROGDIR}/backend/runscrub.sh ${i}" /etc/crontab | awk '{print $5}'`
+   day_month=`grep "${PROGDIR}/backend/runscrub.sh ${i}" /etc/crontab | awk '{print $3}'`
+   if [ "$housr" != '*' -a "$day_week" = '*' -a "$day_month" = '*' ] ; then time="daily @ hour $hour" ; fi
+   if [ "$day_week" != '*' ] ; then time="weekly @ day $day_week" ; fi
+   if [ "$month" != '*' ] ; then time="monthly @ day $day_month @ hour $hour" ; fi
    echo "$i - $time"
 done
-
-#monthly @ day 21 @ hour 13
-#0       22      *       *       *       root    /usr/local/share/lpreserver/backend/runscrub.sh tank
-#0       22      *       *       7       root    /usr/local/share/lpreserver/backend/runscrub.sh tank
-#0       13      21       *       *      root    /usr/local/share/lpreserver/backend/runscrub.sh tank
-
