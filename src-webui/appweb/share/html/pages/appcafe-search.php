2cc5fbd9a5b7dab63daf5c2228ac2f971e0da86f
Kris Moore <kris@pcbsd.org>
2014-09-25 04:59:18 -0400

    Update the search page, mostly ready for real $WORK, just waiting on some changes to syscache

--- src-webui/appweb/share/html/pages/appcafe-search.php
+++ src-webui/appweb/share/html/pages/appcafe-search.php
@@ -4,20 +4,34 @@ defined('DS') OR die('No direct access allowed.');
  if ( empty($_GET['jail']) or ! empty($_GET['changeappcafejail']))
  {
     display_jail_appcafeselection();
- } else {
+    return;
+ }
 
-   if ( ! empty($_GET['search']) )
-     $header="Searching for: ". $_GET['search'];
-   else
-     $header="Package Search";
+ if ( ! empty($_POST['search']) )
+   $header="Searching for: ". $_POST['search'];
+ else
+   $header="Package Search";
+
+ if ( ! empty($_POST['searchtext']) ) {
+    $searchtext = $_POST['searchtext'];
+    $searchraw = $_POST['searchraw'];
+ }
 ?>
 
 <h1><? echo $header; ?></h1>
 <br>
+<form method="post" action="?p=appcafe-search">
+Search: <input name="searchtext" type="text" value="<? echo "$searchtext"; ?>" /> <input name="submit" type="submit" value="Search"/><br>
+Search raw packages: <input name="searchraw" type="checkbox" value="checked" <? if ( $searchraw == "checked") { echo "checked"; } ?> /><br>
+</form>
+
 <?
 
-       if ( $deviceType == "computer" ) { 
-       $totalCols = 3;
+  if ( empty($searchtext) )
+     return;
+
+  if ( $deviceType == "computer" ) { 
+    $totalCols = 3;
 ?>
 <table class="jaillist" style="width:100%">
 <tr>
@@ -26,8 +40,8 @@ defined('DS') OR die('No direct access allowed.');
    <th></th>
 </tr>
 <?
-       } else {
-         $totalCols = 2;
+  } else {
+   $totalCols = 2;
 ?>
 <table class="jaillist" style="width:100%">
 <tr>
@@ -35,5 +49,6 @@ defined('DS') OR die('No direct access allowed.');
    <th></th>
 </tr>
 <?
-       }
+  }
 ?>
+
