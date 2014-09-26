6fe0843995c2d2e9464ddf02bc52b493bc7cd11c
Kris Moore <kris@pcbsd.org>
2014-09-26 04:10:09 -0400

    Add full search capability to AppCafe web, includes ability to search for "raw" pkgng packages.

--- src-webui/appweb/share/html/pages/appcafe-search.php
+++ src-webui/appweb/share/html/pages/appcafe-search.php
@@ -1,9 +1,9 @@
 <?
-defined('DS') OR die('No direct access allowed.');
+ defined('DS') OR die('No direct access allowed.');
 
  if ( empty($_GET['jail']) or ! empty($_GET['changeappcafejail']))
  {
-    display_jail_appcafeselection();
+    display_jail_appcafeselection("appcafe-search");
     return;
  }
 
@@ -20,8 +20,8 @@ defined('DS') OR die('No direct access allowed.');
 
 <h1><? echo $header; ?></h1>
 <br>
-<form method="post" action="?p=appcafe-search">
-Search: <input name="searchtext" type="text" value="<? echo "$searchtext"; ?>" /> <input name="submit" type="submit" value="Search"/><br>
+<form method="post" action="?p=appcafe-search&jail=<? echo "$jailUrl"; ?>">
+Search: <input name="searchtext" type="text" value="<? echo "$searchtext"; ?>" /> <input type="image" style="float: relative;" width="28" height="28" src="images/search.png" alt="Search" /><br>
 Search raw packages: <input name="searchraw" type="checkbox" value="checked" <? if ( $searchraw == "checked") { echo "checked"; } ?> /><br>
 </form>
 
@@ -33,6 +33,7 @@ Search raw packages: <input name="searchraw" type="checkbox" value="checked" <? 
   if ( $deviceType == "computer" ) { 
     $totalCols = 3;
 ?>
+<hr width="100%">
 <table class="jaillist" style="width:100%">
 <tr>
    <th></th>
@@ -40,7 +41,7 @@ Search raw packages: <input name="searchraw" type="checkbox" value="checked" <? 
    <th></th>
 </tr>
 <?
-  } else {
+ } else {
    $totalCols = 2;
 ?>
 <table class="jaillist" style="width:100%">
@@ -49,6 +50,27 @@ Search raw packages: <input name="searchraw" type="checkbox" value="checked" <? 
    <th></th>
 </tr>
 <?
-  }
-?>
+ }
 
+ // Do the search
+ if ( $searchraw == "checked" )
+   $cmd = "pkg search $searchtext $jail 20";
+ else
+   $cmd = "pbi search $searchtext all 20";
+
+ exec("$sc ". escapeshellarg("$cmd"), $pbiarray);
+ $pbilist = explode(", ", $pbiarray[0]);
+
+ // Now loop through pbi origins
+ $col=1;
+ foreach ($pbilist as $pbiorigin) {
+   if ( parse_details($pbiorigin, $jail, $col, true, false) == 0 ) {
+     if ( $col == $totalCols )
+        $col = 1;
+     else
+        $col++;
+   }
+
+ }
+
+?>
