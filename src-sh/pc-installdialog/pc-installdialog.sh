0380e1bbd23a59f95a5daa572e165f8e7c9c7c1a
Josh <joshms@pcbsd.org>
2014-10-08 15:13:24 -0400

    If full disk install is selected from the radiolist the user will now be asked if they want to use MBR or GPT for their disk  layout.

--- src-sh/pc-installdialog/pc-installdialog.sh
+++ src-sh/pc-installdialog/pc-installdialog.sh
@@ -541,7 +541,24 @@ get_target_part()
   if [ -z "$ANS" ] ; then
      exit_err "Invalid disk selected!"
   fi
-  DISKPART="$ANS"
+  #Add a while loop that will prompt for the disk format on a full disk install
+  if [ "ALL" = "$ANS" ] ; then
+     while :
+     do
+	get_dlg_ans "--menu \"Select the disk format you would like to use.\" 12 45 10 1. GPT 2. MBR"
+	if [ -z "$ANS" ] ; then
+	  echo "Invalid disk format entered!" 
+	  continue
+	else
+	  break
+	fi
+    done
+    if [ "1." = "$ANS" ] ; then
+      DISKFORMAT="GPT"
+    else 
+      DISKFORMAT="MBR"
+    fi
+  fi
 }
 
 get_root_pw()
