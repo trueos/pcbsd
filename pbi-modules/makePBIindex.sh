63cbb56ad76b084cc97ead9dd51c1ae5dffafc96
Kris Moore <kris@pcbsd.org>
2014-10-20 10:58:08 -0400

    Add support for local rebuild of PBI index data, which can be used on the local machine immediately. Good for testing, or rebuilding a PBI index when the online index cannot be reached or is stale.

--- pbi-modules/makePBIindex.sh
+++ pbi-modules/makePBIindex.sh
@@ -3,12 +3,35 @@
 # NOTE: Run this script as root from the "pbi-modules" directory in the PC-BSD source tree.
 # This will need to be updated occasionally
 
-#Clean the old index files (comment/uncomment as necessary)
-#rm -rf /var/db/pbi/index/*
+if [ `id -u` != "0" ] ; then
+   echo "Run me as root!"
+   exit 1
+fi
 
-#Copy over the contents of the pbi-modules directory
-cp -Rf * /var/db/pbi/index/.
+if [ ! -e "AppCafe-index" ] ; then
+   echo "Must be run from modules top-level dir..."
+   exit 1
+fi
 
-#Now run pbi_makeindex for each of the PBI modules
-cd /var/db/pbi/index
+echo "This can take 10+ minutes, lots of pkg data to parse..."
+sleep 1
+
+# Run pbi_makeindex now
 pbi_makeindex
+if [ $? -ne 0 ] ; then
+   echo "pbi_makeindex failed!"
+   exit 1
+fi
+
+#Clean the old index files (comment/uncomment as necessary)
+rm -rf /var/db/pbi/index
+
+# Create the new index dir
+mkdir -p /var/db/pbi/index
+
+# Copy over the contents of the pbi-modules directory
+echo "Moving index data to local system..."
+tar xvf PBI-INDEX.txz -C /var/db/pbi/index #2>/dev/null >/dev/null
+
+# Cleanup
+rm PBI-INDEX.txz
