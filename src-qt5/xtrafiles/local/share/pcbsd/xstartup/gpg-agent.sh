f44fa8968c6632e4ba9ecca61d19eeb06f41849b
Kris Moore <kris@pcbsd.org>
2014-12-19 12:45:46 -0500

    Add updated .xprofile, gpg-agent.sh and ssh-agent.sh from Christopher Hall:
    https://bugs.pcbsd.org/issues/7152

--- src-qt5/xtrafiles/local/share/pcbsd/xstartup/gpg-agent.sh
+++ src-qt5/xtrafiles/local/share/pcbsd/xstartup/gpg-agent.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
 # Only start agents if uid is greater than 1000 (regular user)
-# and the .ssh and .gnupg folders are existing in the user's home.
-UID="`id -u`"
+# and the .gnupg folder existd in the user's home.
+# If .ssh folder also exists in the user's home, enable ssh-agent fuctionality.
+
+UID=$(id -u)
 if [ $UID -gt 1000 ]; then
   # GPG-Agent
   if [ -d "${HOME}/.gnupg" ]; then 
     GPG_AGENT="/usr/local/bin/gpg-agent"
     export GPG_TTY=$(tty)
-    if [ -e $GPG_AGENT ] && [ -z "`pgrep -u $UID gpg-agent`" ]; then
-      eval $($GPG_AGENT --daemon --write-env-file "${HOME}/.gpg-agent-info" --log-file "${HOME}/.gnupg/gpg-agent.log")
-    elif [ ! -z "`pgrep -u $UID gpg-agent`" ] && [ -f "${HOME}/.gpg-agent-info" ]; then
-      source "${HOME}/.gpg-agent-info"
-      export GPG_AGENT_INFO
+    gpg_options=
+    [ -d "${HOME}/.ssh" ] && gpg_options=--enable-ssh-support
+    if [ -x "${GPG_AGENT}" ] && [ -z $(pgrep -u "${UID}" gpg-agent) ]; then
+      $GPG_AGENT --daemon ${gpg_options} --log-file "${HOME}/.gnupg/gpg-agent.log" > "${HOME}/.gpg-agent-info"
+      . "${HOME}/.gpg-agent-info"
+    elif [ ! -z $(pgrep -u $UID gpg-agent) ] && [ -f "${HOME}/.gpg-agent-info" ]; then
+      . "${HOME}/.gpg-agent-info"
     fi
+    unset gpg_options
   fi
 fi
-
