7a5bb3d5f55ac34bcc36985c2b669825d799f186
William Katsak <wkatsak@cs.rutgers.edu>
2015-01-23 21:20:22 -0500

    Add warden.conf setting to use NAT for VIMAGE jails
    Disable by default.
    
    Signed-off-by: William Katsak <wkatsak@cs.rutgers.edu>

--- src-sh/warden/scripts/backend/startjail.sh
+++ src-sh/warden/scripts/backend/startjail.sh
@@ -167,49 +167,50 @@ start_jail_vimage()
   fi
 
   #
-  # Set ourself to be a jail router with NAT. Don't
-  # use PF since it will panic the box when used
-  # with VIMAGE.
+  # If enabled in warden.conf, set ourself to be a jail
+  # router with NAT. Don't use PF since it will panic the
+  # box when used with VIMAGE.
   #
-  ip_forwarding=`sysctl -n net.inet.ip.forwarding`
-  if [ "${ip_forwarding}" = "0" ] ; then
-     sysctl net.inet.ip.forwarding=1
-  fi
+  if [ "$NAT_ENABLE" == "true" ]; then
+        ip_forwarding=`sysctl -n net.inet.ip.forwarding`
+        if [ "${ip_forwarding}" = "0" ] ; then
+           sysctl net.inet.ip.forwarding=1
+        fi
 
-  ip6_forwarding=`sysctl -n net.inet6.ip6.forwarding`
-  if [ "${ip6_forwarding}" = "0" ] ; then
-     sysctl net.inet6.ip6.forwarding=1
-  fi
+        ip6_forwarding=`sysctl -n net.inet6.ip6.forwarding`
+        if [ "${ip6_forwarding}" = "0" ] ; then
+           sysctl net.inet6.ip6.forwarding=1
+        fi
 
-  firewall_enable=`egrep '^firewall_enable' /etc/rc.conf|cut -f2 -d'='|sed 's|"||g'`
-  firewall_type=`egrep '^firewall_type' /etc/rc.conf|cut -f2 -d'='|sed 's|"||g'`
+        firewall_enable=`egrep '^firewall_enable' /etc/rc.conf|cut -f2 -d'='|sed 's|"||g'`
+        firewall_type=`egrep '^firewall_type' /etc/rc.conf|cut -f2 -d'='|sed 's|"||g'`
 
-  if [ "${firewall_enable}" != "YES" -o "${firewall_type}" != "open" ] ; then
-     tmp_rcconf=`mktemp /tmp/.wdn.XXXXXX`
+        if [ "${firewall_enable}" != "YES" -o "${firewall_type}" != "open" ] ; then
+           tmp_rcconf=`mktemp /tmp/.wdn.XXXXXX`
      egrep -v '^firewall_(enable|type)' /etc/rc.conf >> "${tmp_rcconf}"
 
      cat<<__EOF__>>"${tmp_rcconf}"
 firewall_enable="YES"
 firewall_type="open"
 __EOF__
-
-     if [ -s "${tmp_rcconf}" ] ; then
-        cp /etc/rc.conf /var/tmp/rc.conf.bak
-        mv "${tmp_rcconf}" /etc/rc.conf
-        if [ "$?" != "0" ] ; then
-           mv /var/tmp/rc.conf.bak /etc/rc.conf
+           if [ -s "${tmp_rcconf}" ] ; then
+              cp /etc/rc.conf /var/tmp/rc.conf.bak
+              mv "${tmp_rcconf}" /etc/rc.conf
+              if [ "$?" != "0" ] ; then
+                 mv /var/tmp/rc.conf.bak /etc/rc.conf
+              fi
+           fi
+           /etc/rc.d/ipfw forcerestart
         fi
-     fi
-     /etc/rc.d/ipfw forcerestart
-  fi
 
-  instance=`get_ipfw_nat_instance "${IFACE}"`
-  if [ -z "${instance}" ] ; then
-     priority=`get_ipfw_nat_priority`
-     instance=`get_ipfw_nat_instance`
+        instance=`get_ipfw_nat_instance "${IFACE}"`
+        if [ -z "${instance}" ] ; then
+           priority=`get_ipfw_nat_priority`
+           instance=`get_ipfw_nat_instance`
 
-     ipfw "${priority}" add nat "${instance}" all from any to any
-     ipfw nat "${instance}" config if "${IFACE}" reset
+           ipfw "${priority}" add nat "${instance}" all from any to any
+           ipfw nat "${instance}" config if "${IFACE}" reset
+        fi
   fi
 # End of jail VIMAGE startup function
 }
