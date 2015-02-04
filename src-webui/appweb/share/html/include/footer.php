d3ef117da07fe34c40c62263c09e680e5fb81b7e
Kris Moore <kris@pcbsd.org>
2015-02-04 10:59:18 -0500

    Fix the AppCafe CSS, get rid of some ugly orange colors, should be a more neutral greyscale now. Maybe Dru will be happier with it???? ;)

--- src-webui/appweb/share/html/include/footer.php
+++ src-webui/appweb/share/html/include/footer.php
@@ -1,14 +1,6 @@
 <? defined('DS') OR die('No direct access allowed.'); ?>
 </div>
 
-<div id="meta" class="dark">
-	<ul>
-		<li><a href="http://www.ixsystems.com" id="copyright" title="Created by iXsystems 2014">iXsystems 2014</a></li>
-		<li>Version: .001</li>
-		<li></li>
-	</ul>
-</div>
-
 <script src="js/main.js"></script>
 <script src="js/doubletaptogo.js"></script>
 <script>
