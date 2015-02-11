b86af3408eba71956fd31c160c9bb9acf6b297bb
Kris Moore <kris@pcbsd.org>
2015-02-11 11:05:09 -0500

    Update Makefile / install.sh for pcbsd-utils, so we can easier add new sub-projects. While here add personacrypt

---
+++ src-sh/pcbsd-utils/install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+DEFAULT="/usr/local"
+
+if [ -z "$1" ] ; then 
+	LB="${DEFAULT}" 
+else
+	LB="${1}" 
+fi
+
+DIR=`dirname $0`
+DIR=`realpath $DIR`
+cd ${DIR}
+
+SUBPROJ="about de-info de-logout pbreg pc-adctl pc-checkxdisplay \
+	 pc-extractoverlay pc-fbsdupdatecheck pc-firstgui pc-installdialog \
+	 pc-pkg pc-runxgui pc-su pc-sysconfig pc-sysinstall pc-systemflag \
+	 pc-thinclient pc-updatemanager pc-xdgutil personacrypt"
+
+# Add all the sub-projects
+for i in ${SUBPROJ}
+do
+  cd $i
+  if [ $? -ne 0 ] ; exit 1 ; fi
+  make install
+  if [ $? -ne 0 ] ; exit 1 ; fi
+  cd ${DIR}  
+  if [ $? -ne 0 ] ; exit 1 ; fi
+done
