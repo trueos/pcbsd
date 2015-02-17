7d17f079b602aeea863c218754ca97f5d2195c98
Kris Moore <kris@pcbsd.org>
2015-02-17 09:42:20 -0500

    Fix bug #8038, the vbox move script

--- src-qt5/xtrafiles/local/share/pcbsd/scripts/move-vbox.sh
+++ src-qt5/xtrafiles/local/share/pcbsd/scripts/move-vbox.sh
@@ -3,6 +3,17 @@
 # 1 = USER
 # 2 = HOME
 
+# Move an existing old VM dirs in users home
+if [ -d "${2}/VirtualBox VMs" ] ; then
+   mv "${2}/VirtualBox VMs" "${2}/VirtualBox VMs.old"
+fi
+if [ -d "${2}/.VirtualBox" ] ; then
+   mv "${2}/.VirtualBox" "${2}/.VirtualBox.old"
+fi
+if [ -d "${2}/.config/VirtualBox" ] ; then
+   mv "${2}/.config/VirtualBox" "${2}/.config/VirtualBox.old"
+fi
+
 # Helper script to move VBOX from /root -> /usr/home/FOO
 if [ -e "/usr/local/bin/kdialog" ] ; then
   dbusRef=`kdialog --progressbar "Moving VM's... Please wait..." 0`
@@ -11,7 +22,16 @@ if [ -e "/usr/local/bin/kdialog" ] ; then
 elif [ -e "/usr/local/bin/zenity" ] ; then
   mv "/root/VirtualBox VMs" "${2}/VirtualBox VMs" | zenity --text "Moving VMs... Please wait..." --progress --pulsate --auto-close --no-cancel
 fi
-chown -R "${1}:${1}" "${2}/VirtualBox VMs"
+
+# Fix the snapshots / paths in vbox files
+cd "${2}/VirtualBox VMs"
+if [ $? -eq 0 ] ; then
+  for vfile in `find . | grep \.vbox$`
+  do
+    sed -i '' "s|/root/|${2}/|g" "${vfile}"
+  done
+  chown -R "${1}:${1}" "${2}/VirtualBox VMs"
+fi
 
 if [ -d "/root/.VirtualBox" ] ; then
   mv "/root/.VirtualBox" "${2}/.VirtualBox"
