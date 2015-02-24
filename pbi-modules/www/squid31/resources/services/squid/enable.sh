d418cb5c078a998c9c750a3da1798e4a0e39e0e4
JoshDW19 <joshms@pcbsd.org>
2015-02-24 12:33:47 -0500

    2nd Half of the PBI tree pruning.  Removed / updated / changed failing PBIs

--- pbi-modules/www/squid31/resources/services/squid/enable.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# This script enables the service at bootup
-# Return 0 on success, or 1 on failure. 
-
-cat /etc/rc.conf | grep -v "squid_enable=" >/tmp/rc.new
-echo "squid_enable=\"YES\"" >> /tmp/rc.new
-mv /tmp/rc.new /etc/rc.conf
-
-return 0
