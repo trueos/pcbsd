c8d679af3bd2e6976b02b58fbd3a1ac117c3b4fa
dlavigne <dru@freebsd.org>
2015-03-20 13:14:11 -0400

    Add README and fix error in config file.

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinxcontrib.httpdomain', 'sphinx.ext.numfig']
+extensions = ['sphinxcontrib.httpdomain']
 
 #numfig:
 #numfig = True
