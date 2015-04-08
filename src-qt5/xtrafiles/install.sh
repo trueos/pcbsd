cd95bf490755140081904fcc309db908595571dc
Ken Moore <ken@pcbsd.org>
2015-04-08 16:38:18 -0400

    make sure that the /usr/local/share/pcbsd/xstartup directorie is cleaned out before applying the new file overlay for the src-qt5 package. This should fix the issue with leftover *.sh files causing the tray apps to get started twice (with the new XDG autostart format being the other one).

--- src-qt5/xtrafiles/install.sh
+++ src-qt5/xtrafiles/install.sh
@@ -10,6 +10,12 @@ fi
 
 echo "Installing extra files to ${LB}"
 
+#Make sure to clean up certain directories before applying the overlay
+if [ -e ${LB}/share/pcbsd/xstartup ]; then
+  echo " - Cleaning ${LB}/share/pcbsd/xstartup first"
+  rm ${LB}/share/pcbsd/xstartup/*.sh
+fi
+
 # Copy files to system
 tar cvf - -C local . 2>/dev/null | tar xvf - -C ${LB} 2>/dev/null
 if [ $? -ne 0 ] ; then
