33b1a8ce09c7e4f360709330bcdeb830d99e6fcd
Ken Moore <ken@pcbsd.org>
2015-04-15 16:34:58 -0400

    Add a new script to /usr/local/share/pcbsd/scripts: waitforsystray.sh This basically just probes the system for any window which advertises itself as a system tray before exiting. If it does not find a tray, it sleeps for 10 seconds before trying again (and again, and again....)

---
+++ src-qt5/xtrafiles/local/share/pcbsd/scripts/waitforsystray.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#This is a simple script to wait until a system tray is registered before continuing on
+
+ready="FALSE"
+while [ ${ready} == "FALSE" ]
+do
+  #The system tray is registered with the _NET_SYSTEM_TRAY_S1 atom, and *should* be a child of the root window
+  #First get all the window ID's
+  for i in `xwininfo -root -tree | grep + | grep 0x`
+  do
+    #This goes word by word (at the moment)
+    #Pull the ID out of this line (first word is the Hex ID)
+    #echo "Check Line: ${i}"
+    #WID=`echo "${i}" | cut -w -f 1,1`
+    WID="${i}"
+    TMP=`echo "${WID}" | grep 0x | grep -v +`
+    if [ "x${TMP}" == "x" ]; then
+      continue #invalid WID
+    fi
+    #echo "Found WID: $WID"
+    #Now scan that window for the atom
+    TMP=`xprop -id $WID | grep "_NET_SYSTEM_TRAY_VISUAL(VISUALID)"`
+    #echo "INFO: ${TMP}"
+    if [ ! "x" == "x${TMP}" ]; then
+      echo "Found System Tray";
+      ready="TRUE"
+      break
+    fi
+  done
+  if [ ${ready} == "FALSE" ]; then
+    sleep 10; #wait 10 seconds before trying again
+  fi
+done
