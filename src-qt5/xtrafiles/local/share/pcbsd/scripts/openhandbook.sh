93d4b4eb03dc1bc6e41d64708b865fc6c6d2b977
Ken Moore <ken@pcbsd.org>
2015-04-23 13:52:55 -0400

    Also update the "openhandbook.sh" script to use the new pc-webkitviewer utility (for backwards compatibility only - this will not be used by the new system shortcuts). The one downside to this routine is that the title for the window is not translatable.

--- src-qt5/xtrafiles/local/share/pcbsd/scripts/openhandbook.sh
+++ src-qt5/xtrafiles/local/share/pcbsd/scripts/openhandbook.sh
@@ -1,31 +1,33 @@
 #!/bin/sh
 
-hDir="/usr/local/share/pcbsd/doc"
-hFile="handbook"
+pc-webkitviewer -t "PC-BSD Handbook" -i /usr/local/share/pcbsd/icons/pcbsd-handbook.png /usr/local/share/pcbsd/docs/html/pcbsd.html
 
-apps="okular mupdf xpdf"
+#hDir="/usr/local/share/pcbsd/doc"
+#hFile="handbook"
+
+#apps="okular mupdf xpdf"
 
 # Find a valid PDF viewer
-for app in $apps
-do
-	which $app >/dev/null 2>/dev/null
-	if [ $? -eq 0 ] ; then
-		viewer=$app
-		break
-	fi
-done
+#for app in $apps
+#do
+#	which $app >/dev/null 2>/dev/null
+#	if [ $? -eq 0 ] ; then
+#		viewer=$app
+#		break
+#	fi
+#done
 
-if [ -z "$viewer" ] ; then
-	echo "Error: No PDF viewer found."
-	exit 1
-fi
+#if [ -z "$viewer" ] ; then
+#	echo "Error: No PDF viewer found."
+#	exit 1
+#fi
 
-if [ -z "$LANG" ] ; then
-	$viewer ${hDir}/${hFile}_en.pdf
-else
-	if [ -e "${hDir}/${hFile}_${LANG}.pdf" ] ; then
-		$viewer ${hDir}/${hFile}_${LANG}.pdf
-	else
-		$viewer ${hDir}/${hFile}_en.pdf
-	fi
-fi
+#if [ -z "$LANG" ] ; then
+#	$viewer ${hDir}/${hFile}_en.pdf
+#else
+#	if [ -e "${hDir}/${hFile}_${LANG}.pdf" ] ; then
+#		$viewer ${hDir}/${hFile}_${LANG}.pdf
+#	else
+#		$viewer ${hDir}/${hFile}_en.pdf
+#	fi
+#fi
