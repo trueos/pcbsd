d7b2af6f74216a28c030169dd5dc6230dd62095e
Kris Moore <kris@pcbsd.org>
2015-05-06 09:55:58 -0400

    Opps, fix typo

--- src-qt5/warden-gui/main.cpp
+++ src-qt5/warden-gui/main.cpp
@@ -18,7 +18,7 @@ int main( int argc, char ** argv )
     QString langCode = mylocale.name();
     if ( ! QFile::exists( "/usr/local/share/pcbsd/i18n/Warden_" + langCode + ".qm" ) )
       langCode.truncate(langCode.indexOf("_"));
-    translator.load( QString("Warden_") + langCode, "/usr/local/pcbsd/warden/i18n/" );
+    translator.load( QString("Warden_") + langCode, "/usr/local/share/pcbsd/i18n/" );
     a.installTranslator( &translator );
     qDebug() << "Locale:" << langCode;
     QTextCodec::setCodecForLocale( QTextCodec::codecForName("UTF-8") ); //Force Utf-8 compliance
