def603a53153213665382eced9a1c48b5155f034
Kris Moore <kris@pcbsd.org>
2015-05-28 10:50:58 -0400

    Fix a src bug with bad braces and a missing {...
    Pointyhat: kenmoore

--- src-qt5/pc-controlpanel/backend/cp-item.cpp
+++ src-qt5/pc-controlpanel/backend/cp-item.cpp
@@ -351,28 +351,22 @@ void CControlPanelItem::launch(bool useInternalLaunch)
     }
     else
     {
-        /*pid_t RetVal = fork();
-        if (!RetVal)
-        {*/
-	    QProcess proc;
-            if (mExecPath.length())
-            {
-                //chdir(qPrintable(mExecPath));
-		proc.setWorkingPath(mExecPath);
-            }
-            QString Str = mExecCommand;
-            Str.replace("%i", QString(" --icon ") + mIconFile);
-            Str.replace("%c", QString("\"") + displayName().toLocal8Bit() + QString("\""));
-            Str.replace("%k", mFile);
-            Str.replace("%f","");
-            Str.replace("%F","");
-            Str.replace("%u","");
-            Str.replace("%U","");
-            //exit (system(Str.toLatin1()));
-	    proc.startDetached(Str);
-         }
+        QProcess proc;
+        if (! mExecPath.isEmpty() )
+        {
+           proc.setWorkingDirectory(mExecPath);
+        }
+        QString Str = mExecCommand;
+        Str.replace("%i", QString(" --icon ") + mIconFile);
+        Str.replace("%c", QString("\"") + displayName().toLocal8Bit() + QString("\""));
+        Str.replace("%k", mFile);
+        Str.replace("%f","");
+        Str.replace("%F","");
+        Str.replace("%u","");
+        Str.replace("%U","");
+        //exit (system(Str.toLatin1()));
+        proc.startDetached(Str);
     }//if internal launch
-
     
 }
 
