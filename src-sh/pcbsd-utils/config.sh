40aeca6c3774a92840aa77acae316fdebe723d7a
Kris Moore <kris@pcbsd.org>
2015-07-09 10:38:37 -0400

    Hopefully last test of Jenkins build / status on Github

--- src-sh/pcbsd-utils/config.sh
+++ src-sh/pcbsd-utils/config.sh
@@ -5,6 +5,6 @@
 # When you update this tag, be sure to update ../src-qt4/config.h as well
 export PCBSDVERSION="master"
 
-# When rolling ISOs, what package set to use on first install?
+# When rolling ISOs, what package set to use on initial install?
 # {PRODUCTION|EDGE|ENTERPRISE}
 export INSTALLPACKAGESET="EDGE"
