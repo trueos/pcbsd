0018493c23b5cf681044da223f410deae8aa3f44
JoshDW19 <joshms@pcbsd.org>
2015-07-25 16:06:19 -0400

    Add a CSS class for package descriptions so they don't have to follow the same rules as the table grid / list for PBIs

--- src-webui/appweb/share/html/pages/appinfo.php
+++ src-webui/appweb/share/html/pages/appinfo.php
@@ -563,7 +563,7 @@ function display_app_link($pbilist, $jail)
 ?>
    
 <br>
-<table class="jaillist" style="width:<?php if ( $deviceType == "computer" ) { echo "600px"; } else { echo "100%"; } ?>">
+<table class="pbidescription" style="width:<?php if ( $deviceType == "computer" ) { echo "600px"; } else { echo "100%"; } ?>">
   <tr>
     <th colspan=3>
       <?php 
