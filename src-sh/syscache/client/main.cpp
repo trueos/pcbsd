b3e548bf6b8843cb7b6bd48768534d5619ecb867
Ken Moore <moorekou@gmail.com>
2015-07-28 13:09:30 -0400

    Also update the help page to note the new output syntax for the "jail cages" command

--- src-sh/syscache/client/main.cpp
+++ src-sh/syscache/client/main.cpp
@@ -14,7 +14,7 @@ void printJailUsage(){
   qDebug() << "syscache Jail database access:\n\
 -------------------------------------------------------------------------------\n\
 \"jail list\": List all running jails by name\n\
-\"jail cages\": List all installed pbicages by origin\n\
+\"jail cages\": List all installed pbicages by origin and iocage ID (<origin> <ID>)\n\
 \"jail stoppedlist\": List all stopped jails by name\n\
 \"jail <jailname> <info>\": Get information about a particular jail\n\
   Possible Info requests:\n\
