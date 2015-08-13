39563c80c47cd27358dfd0d248d3af4269c2498f
Kris Moore <kris@pcbsd.org>
2015-08-13 11:51:50 -0400

    Tag RC2

--- src-sh/pcbsd-utils/config.sh
+++ src-sh/pcbsd-utils/config.sh
@@ -3,7 +3,7 @@
 
 # Set the PC-BSD version number
 # When you update this tag, be sure to update ../src-qt4/config.h as well
-export PCBSDVERSION="10.2-RC1"
+export PCBSDVERSION="10.2-RC2"
 
 # When rolling ISOs, what package set to use on initial install?
 # {PRODUCTION|EDGE|ENTERPRISE}
