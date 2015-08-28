65697516dbb769710aec170853636320cb70a24c
Kris Moore <kris@pcbsd.org>
2015-08-28 12:16:57 -0400

    Fix bug #11097
    This is a bug where it looks like we need to run "gmirror destroy"
    before doing a new label, to ensure it that backup meta-data is
    properly cleared beforehand.

--- src-sh/pcbsd-utils/config.sh
+++ src-sh/pcbsd-utils/config.sh
@@ -3,8 +3,8 @@
 
 # Set the PC-BSD version number
 # When you update this tag, be sure to update ../src-qt4/config.h as well
-export PCBSDVERSION="master"
+export PCBSDVERSION="10.2"
 
 # When rolling ISOs, what package set to use on initial install?
 # {PRODUCTION|EDGE|ENTERPRISE}
-export INSTALLPACKAGESET="EDGE"
+export INSTALLPACKAGESET="PRODUCTION"
