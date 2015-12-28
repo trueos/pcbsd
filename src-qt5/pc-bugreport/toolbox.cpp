6a1fd2174dedda2f0cd388cce603c2e7f528782d
Ken Moore <moorekou@gmail.com>
2015-12-28 11:58:53 -0500

    Clean up the diagnostic sheet generation process in pc-bugreport a bit. Now it should be accurate about success/failure all the time.

--- src-qt5/pc-bugreport/toolbox.cpp
+++ src-qt5/pc-bugreport/toolbox.cpp
@@ -76,14 +76,15 @@ void Toolbox::on_actionDiagnostic_report_triggered()
 	//Generate the diagnostic file on the user desktop
 	QString filename = QDir::homePath()+"/Desktop/diagnostic-"+QDate::currentDate().toString("yyyyMMdd")+".txt";
 	QString username = getenv("LOGNAME");
-	QString cmd = "/usr/local/share/pcbsd/scripts/GenDiagSheet.sh "+filename+" "+username;
+	QString cmd = "/usr/local/share/pcbsd/scripts/GenDiagSheet.sh \""+filename+"\" \""+username+"\"";
 	QProcess p;
 	p.setProcessEnvironment(QProcessEnvironment::systemEnvironment());
 	//qDebug() << " - cmd:" << cmd;
 	if(QFile::exists(filename)){ QFile::remove(filename); } //remove any old file first
 	p.start(cmd);
-	while(p.waitForFinished(4000)){
+	while(p.state()!=QProcess::NotRunning ){
 	  QApplication::processEvents();
+	  usleep(300000); //300 ms sleep between updates
 	}
 	if(QFile::exists(filename)){
 	  qDebug() << " - success";
