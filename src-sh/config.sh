d782b498f23bd8e19295794545b73398ee4801c7
Mrt134 <timmoore88@gmail.com>
2016-04-19 11:13:22 -0400

    Fix the symlinks we broke during copy

--- src-sh/config.sh
+++ src-sh/config.sh
@@ -1,10 +1,1 @@
-#!/bin/sh
-# Configuration options for src-sh applications
-
-# Set the PC-BSD version number
-# When you update this tag, be sure to update ../src-qt4/config.h as well
-export PCBSDVERSION="master"
-
-# When rolling ISOs, what package set to use on initial install?
-# {PRODUCTION|EDGE|ENTERPRISE}
-export INSTALLPACKAGESET="EDGE"
+pcbsd-utils/config.sh
