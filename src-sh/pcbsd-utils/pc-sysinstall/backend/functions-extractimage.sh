4726cc5bce1c9f71f6e6feace5bc63b00725488f
Joe Maloney <pkgdemonteam@gmail.com>
2016-04-24 23:06:29 -0400

    Fix / permissions after install from LiveCD method
    This is needed to fix issues after rsync from RO layer.

--- src-sh/pcbsd-utils/pc-sysinstall/backend/functions-extractimage.sh
+++ src-sh/pcbsd-utils/pc-sysinstall/backend/functions-extractimage.sh
@@ -163,6 +163,7 @@ start_extract_uzip_tar()
       mount -o ro /dev/${DEVICE}.uzip ${CDMNT}${UZIP_DIR}
 
       rsync -avH --exclude 'media/*' --exclude 'proc/*' --exclude 'mnt/*' --exclude 'tmp/*' --exclude 'dist/*' --exclude 'gbi' --exclude 'cdmnt-install' ${CDMNT}${UZIP_DIR} ${FSMNT}/
+      chmod 755 ${FSMNT}/
       if [ "$?" != "0" ]
       then
         umount -f ${CDMNT}${UZIP_DIR}
