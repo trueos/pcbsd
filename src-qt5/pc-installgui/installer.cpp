ff72baa8a14eb93931ad576ea7f1c708d53a3b87
Kris Moore <kris@pcbsd.org>
2016-05-03 11:46:45 -0400

    Add lumina-i18n package to installer

--- src-qt5/pc-installgui/installer.cpp
+++ src-qt5/pc-installgui/installer.cpp
@@ -1506,7 +1506,7 @@ QStringList Installer::getDeskPkgCfg()
    if ( radioDesktop->isChecked() ) {
      // Our default list of packages that makeup a desktop
      // This is always able to be changed by user post-install
-     pkgList << "misc/pcbsd-base" << "x11/lumina";
+     pkgList << "misc/pcbsd-base" << "x11/lumina" << "x11/lumina-i18n";
 
      // If using GRUB, make sure the pkgs get loaded
      if ( bootLoader == "GRUB" )
