26810d8e0d803a22e1a6b97f08fb0333ee1a2a22
Ken Moore <moorekou@gmail.com>
2016-06-13 09:16:27 -0400

    Clean up which packages are included/installed by default.

--- src-qt5/pc-installgui/installer.cpp
+++ src-qt5/pc-installgui/installer.cpp
@@ -1509,28 +1509,28 @@ QStringList Installer::getDeskPkgCfg()
        pkgList << "sysutils/grub2-pcbsd" << "sysutils/grub2-efi";
 
      // The default web-browser and plugins
-     pkgList << "www/firefox" << "java/icedtea-web" << "www/qupzilla-qt5";
+     pkgList <<  "www/qupzilla-qt5";
 
      // The default mail client
-     pkgList << "mail/thunderbird" << "mail/trojita";
+     pkgList << "mail/trojita";
 
      // Multimedia player
      pkgList << "multimedia/vlc" << "multimedia/openh264";
 
      // VirtualBox
-     pkgList << "misc/pcbsd-meta-virtualbox";
+     //pkgList << "misc/pcbsd-meta-virtualbox";
 
      // Office Suite
-     pkgList << "editors/libreoffice";
+     //pkgList << "editors/libreoffice";
 
      // Utilities
-     pkgList << "archivers/unrar" << "archivers/unzip" << "archivers/peazip" << "editors/vim";
+     pkgList << "archivers/unrar" << "archivers/unzip";
 
      // Printer packages
      pkgList << "print/cups-pdf";
 
      // Include i18n stuff?
-     if ( comboLanguage->currentIndex() != 0 )
+     //if ( comboLanguage->currentIndex() != 0 )
        pkgList << "misc/pcbsd-i18n" << "misc/pcbsd-i18n-qt5";
 
      // Check if we are using NVIDIA driver and include it automatically
